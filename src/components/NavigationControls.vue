e94bbbd8d606393b764dcc150ff651808966abff
vanilla-ice <valeraerohin97@mail.ru>
2018-07-12 19:38:49 +0300

    Feature: add team page

--- src/components/NavigationControls.vue
+++ src/components/NavigationControls.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="navigation" :style="{minHeight: topImage ? '25px' : 'initial'}">
-    <img @click="$router.go(-1)" v-if="back" src="@/assets/back.png" alt="">
+    <img @click="$router.go(-1)" v-if="back && !white" src="@/assets/back.png" alt="">
+    <img @click="$router.go(-1)" v-if="back && white" src="@/assets/back-white.png" alt="">
     <!--<div class="steps">-->
       <!--<div v-for="(step, index) in steps" :key="index">-->
         <!--<img v-if="step && step.active" class='step-img' src="@/assets/active-step-dash.svg" alt="">-->
@@ -12,7 +13,8 @@
       v-if="topImage"
       :style="{ backgroundImage: `url(${topImage}` }"
     />
-    <img v-if="menu" src="@/assets/big_dots.png" alt="" @click="$emit('menuClick')">
+    <img v-if="menu && !white" src="@/assets/big_dots.png" alt="" @click="$emit('menuClick')">
+    <img class="share-img" v-if="white" src="@/assets/share.png" alt="">
     <img v-if="close" src="@/assets/close-icon.svg" alt="" @click="$emit('closeClick')">
   </div>
 </template>
@@ -28,7 +30,8 @@ export default {
     'close': {
       default: false
     },
-    'topImage': String
+    'topImage': String,
+    'white': Boolean
   },
 }
 </script>
@@ -52,9 +55,15 @@ export default {
   justify-content: space-between;
   margin-top: 20px;
   position: relative;
+  z-index: 1;
   img {
     height: 17px;
     width: auto;
+  }
+
+  .share-img {
+    width: 17px;
+    height: 22px;
   }
 }
 
@@ -73,5 +82,6 @@ export default {
   left: 50%;
   transform: translate(-50%, -25%);
 }
+
   
 </style>
